e2ec0f664d195a4426eb12fbdbb92af7a6ccac74
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-01-16 23:12:56 +0530

    guessing

---
+++ Guess_game.py
@@ -0,0 +1,19 @@
+the_word = "winner"
+guess = ""
+guess_count = 0
+guess_limit = 5
+out_of_guesses = False
+
+
+while guess != the_word and not(out_of_guesses):
+  if guess_count < guess_limit:
+    guess = input("Guesss the word: ")
+    guess_count += 1
+  else:
+    out_of_guesses = True
+
+
+if out_of_guesses:
+  print("you lost the game")
+else:
+  print("you won")
